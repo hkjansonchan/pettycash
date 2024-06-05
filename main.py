d8a837c2b2bbf5033f1327cf31ac32324d0e866c
Janson <hkjansonchan@gmail.com>
2024-06-05 17:10:42 +0800

    update main.py

--- main.py
+++ main.py
@@ -1,5 +1,83 @@
 import pandas as pd
+import openpyxl
 
-file = r"C:\Users\hkjan\Downloads\IvyWalletReport-20240604-2215.csv"
-df = pd.read_csv(file)
-print(df.to_string())
+
+def syb(x: int, y: int, a: int):
+    if sheet.cell(x, y).value == None:
+        sheet.cell(x, y).value = f"={a}"
+    else:
+        sheet.cell(x, y).value += f"+{a}"
+
+
+def dr(d):
+    dat = pd.Timestamp(d.iat[r, 0])
+    amount = d.iat[r, 4]
+    dy = dat.day
+    match d.iat[r, 3]:
+        case "Cash":
+            syb(dy, 2, amount)
+        case "O-Card":
+            syb(dy, 4, amount)
+        case "Saving_HSBC":
+            syb(dy, 6, amount)
+        case "O-Card Samsung Pay":
+            syb(dy, 6, amount)
+
+
+def cr(d):
+    dat = pd.Timestamp(d.iat[r, 0])
+    amount = d.iat[r, 4]
+    dy = dat.day
+    match d.iat[r, 3]:
+        case "Cash":
+            syb(dy, 3, amount)
+        case "O-Card":
+            syb(dy, 5, amount)
+        case "Saving_HSBC":
+            syb(dy, 7, amount)
+        case "O-Card Samsung Pay":
+            syb(dy, 7, amount)
+
+
+r = 0
+file = r"C:\Users\hkjan\Desktop\May.xlsx"
+df = pd.read_excel(file)
+df = df.iloc[:, 0:12]
+wb = openpyxl.Workbook()
+sheet = wb.worksheets[0]
+while r < len(df):
+    date = pd.Timestamp(df.iat[r, 0])
+    if date.month != 5:
+        continue
+    dy = date.day
+    amount = df.iat[r, 4]
+    match df.iat[r, 6]:
+        case "INCOME":
+            sheet.cell(date.day, 1).value = df.iat[r, 4]
+            dr(df)
+        case "EXPENSE":
+            cr(df)
+            match df.iat[r, 2]:
+                case "Shopping":
+                    syb(dy,8,amount)
+                case "Food & Drinks":
+                    syb(dy,9,amount)
+                case "Transport":
+                    syb(dy,10,amount)
+                case "Bills & Fees":
+                    syb(dy,11,amount)
+                case "Entertainment":
+                    syb(dy,12,amount)
+        case "TRANSFER":
+            cr(df)
+            match df.iat[r, 9]:
+                case "Cash":
+                    syb(dy,2,amount)
+                case "O-Card":
+                    syb(dy,4,amount)
+                case "Saving_HSBC":
+                    syb(dy,6,amount)
+                case "O-Card Samsung Pay":
+                    syb(dy,6,amount)
+    r += 1
+wb.save(r"C:\Users\hkjan\Desktop\test2.xlsx")
